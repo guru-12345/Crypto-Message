05456e96f17e6adc80c2fc17747710e08fb3dee6
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-05-16 10:33:59 +0530

    Update 5.1 - Building a Simple Window for Crypto Message.py

--- 5.1 - Building a Simple Window for Crypto Message.py
+++ 5.1 - Building a Simple Window for Crypto Message.py
@@ -1,28 +1,8 @@
 #Task 1: Import the customtkinter module as ctk.
-#Task 2: Create a variable 'background' and set it to a dark color code.
+#Task 2: Create a variable 'background' and set it to color code "#2E2E2E" (Hexacolor code).
 #Task 3: Create the main application window using CTk.
 #Task 4: Set the window size to 600x450 using .geometry().
 #Task 5: Allow the window to be resizable by passing True, True.
-#Task 6: Set the background color using .config() method.
+#Task 6: Set the background color using .config(bg=background) method.
 #Task 7: Set the window title to "Message Encoder".
 #Task 8: Use .mainloop() to keep the window running.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
