f647fc09528db4f728b9bc705be152893f11bde3
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-07-01 10:54:20 +0530

    Update 8.2  Introduction to coordinates.py

--- 8.2  Introduction to coordinates.py
+++ 8.2  Introduction to coordinates.py
@@ -1,3 +1,6 @@
+#In this code, labels are used to display text at specific (x, y) coordinates on the window using the place() method.
+#For example, (0, 0) places the label at the top-left corner, and (300, 225) places it near the center of a 600×450 window.
+
 #Run the code and observe the x and y coordinates of label1 and label2.
 #Task-1: Create and place new label at coordinates (430, 430) in the bottom-right corner.
 
@@ -6,7 +9,7 @@ import customtkinter as ctk
 # Create the main window
 root = ctk.CTk()
 root.geometry("600x450")  # Window size 600x450
-
+root.bgcolor("#2E2E2E")
 # Add a label at the top-left corner (0, 0)
 label1 = ctk.CTkLabel(root, text="Top-left Corner")
 label1.place(x=0, y=0)
